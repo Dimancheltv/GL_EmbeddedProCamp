945ec96e0bbf611a8063f648047c0e2f341ee3cc
Brave_Engineer <the.polyhedral@gmail.com>
2020-01-03 12:39:52 +0200

    fix code-style

--- lecture5/Exercise_1/src/uart.c
+++ lecture5/Exercise_1/src/uart.c
@@ -3,28 +3,28 @@
 
 void init(void)
 {
-		/*! USART clock */
-		RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
-		/*! GPIOC clock */
-		RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
-    
-    /*! TX setup pc4 */
-    GPIOC->MODER |= GPIO_MODER_MODER4_1; 
-    GPIOC->OTYPER &= ~GPIO_OTYPER_OT_4; 
-    GPIOC->PUPDR &= ~GPIO_PUPDR_PUPDR4; 
-    GPIOC->PUPDR |= GPIO_PUPDR_PUPDR4_0;
-	  GPIOC->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR4;
-    GPIOC->AFR[0]&= ~GPIO_AFRL_AFRL4;
-    GPIOC->AFR[0]|= 7 << (4 * 4);
-    
-    /*! RX setup pc5 */
-    GPIOC->MODER |= GPIO_MODER_MODER5_1;
-    GPIOC->PUPDR &= ~GPIO_PUPDR_PUPDR5;
-    GPIOC->AFR[0]&= ~GPIO_AFRL_AFRL5;
-    GPIOC->AFR[0]|= 7 << (5 * 4);
-	
-		/*! UASRT baud_rate */
-		USART1->BRR = 8000000 / BAUD_RATE;
+        /*! USART clock */
+        RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+        /*! GPIOC clock */
+        RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+        
+        /*! TX setup pc4 */
+        GPIOC->MODER |= GPIO_MODER_MODER4_1; 
+        GPIOC->OTYPER &= ~GPIO_OTYPER_OT_4; 
+        GPIOC->PUPDR &= ~GPIO_PUPDR_PUPDR4; 
+        GPIOC->PUPDR |= GPIO_PUPDR_PUPDR4_0;
+          GPIOC->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR4;
+        GPIOC->AFR[0]&= ~GPIO_AFRL_AFRL4;
+        GPIOC->AFR[0]|= 7 << (4 * 4);
+        
+        /*! RX setup pc5 */
+        GPIOC->MODER |= GPIO_MODER_MODER5_1;
+        GPIOC->PUPDR &= ~GPIO_PUPDR_PUPDR5;
+        GPIOC->AFR[0]&= ~GPIO_AFRL_AFRL5;
+        GPIOC->AFR[0]|= 7 << (5 * 4);
+        
+        /*! UASRT baud_rate */
+        USART1->BRR = 8000000 / BAUD_RATE;
 }
 
 void open(void) 
